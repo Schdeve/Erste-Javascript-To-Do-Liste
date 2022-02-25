280ff771ba2dd4ef22784f0c3937f4657687a308
stefan.keller <skeller2@gmx.net>
2022-02-25 11:19:55 +0100

    updates

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ const toDosList = document.getElementById("offeneAufgaben");
 const toDoForm = document.getElementById("eingabeFeld");
 const done = document.getElementById("erledigteAufgaben")
 
-toDoForm.querySelector("button").addEventListener("click", function() {
+toDoForm.querySelector("button").addEventListener("click", function() {   
   const toDoItem = document.createElement("li");
   toDoItem.textContent = toDoForm.querySelector("input").value;
   toDosList.appendChild(toDoItem);
+  
   const removeButton = document.createElement("button");
   removeButton.textContent = "Löschen!";
   removeButton.setAttribute("type", "button");
-  removeButton.onclick = function() { this.parentElement.remove(); }
+  removeButton.onclick = function() { 
+    this.parentElement.remove();
+  }
   toDoItem.appendChild(removeButton);
+
   const doneButton = document.createElement("button")
-  doneButton.textContent = "Erledigt"
+  doneButton.textContent = "Erledigt!"
   doneButton.setAttribute("type", "button")
   doneButton.onclick = function() { 
       this.parentElement.remove();
